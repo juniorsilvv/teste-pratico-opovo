aeedced2032e98e220c0d51e42ace6e2976e8afd
juniorsilva <hjuniorbsilva@gmail.com>
2022-09-03 17:36:29 -0300

    ajustes, adicionado comentários

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -22,40 +22,33 @@ $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
 
-$routes->get('/', function(){
-    session_start();
-    var_dump($_SESSION);
-});
-$routes->group('api', function($routes) {
-        $routes->post('register', 'WebController::register');
-        $routes->post('login', 'WebController::login');
+$routes->group('api', function ($routes) {
+    // Registro de jornalista
+    $routes->post('register', 'WebController::register');
+    // Login de jornalista
+    $routes->post('login', 'WebController::login');
 
-        //Retorna os dados do jornalista
-        $routes->get('me', 'JournalistController::journalistMe', ['filter' => 'Auth']);
+    // Retorna os dados do jornalista
+    $routes->get('me', 'JournalistController::journalistMe', ['filter' => 'Auth']);
+    // Lista os tipos de notícia
+    $routes->get('type/me', 'NewsTypeController::newsTypeMe', ['filter' => 'Auth']);
+    // Cria um tipo de noticia
+    $routes->post('type/create', 'NewsTypeController::create', ['filter' => 'Auth']);
+    // Altera tipo de notícia
+    $routes->put('type/update/(:num)', 'NewsTypeController::update/$1', ['filter' => 'Auth']);
+    // Deleta Tipo de notícia
+    $routes->delete('type/delete/(:num)', 'NewsTypeController::delete/$1', ['filter' => 'Auth']);
 
-        // Lista os tipos de notícia
-        $routes->get('type/me', 'NewsTypeController::newsTypeMe', ['filter' => 'Auth']);
-        // Cria um tipo de noticia
-        $routes->post('type/create', 'NewsTypeController::create', ['filter' => 'Auth']);
-        // Altera tipo de notícia
-        $routes->put('type/update/(:num)', 'NewsTypeController::update/$1', ['filter' => 'Auth']);
-        // Deleta Tipo de notícia
-        $routes->delete('type/delete/(:num)', 'NewsTypeController::delete/$1', ['filter' => 'Auth']);
-
-        // Lista as notícias
-        $routes->get('news/me', 'NewsController::newsMe', ['filter' => 'Auth']);
-        // Lista as notícias por tipo
-        $routes->get('news/me/(:num)', 'NewsController::newsTypeMe/$1', ['filter' => 'Auth']);
-        // Cria a notícia
-        $routes->post('news/create', 'NewsController::create', ['filter' => 'Auth']);
-        // Altera a notícia
-        $routes->put('news/update/(:num)', 'NewsController::update/$1', ['filter' => 'Auth']);
-        // Deleta Tipo de notícia
-        $routes->delete('news/delete/(:num)', 'NewsController::delete/$1', ['filter' => 'Auth']);
-
-        
-
-
+    // Lista as notícias
+    $routes->get('news/me', 'NewsController::newsMe', ['filter' => 'Auth']);
+    // Lista as notícias por tipo
+    $routes->get('news/me/(:num)', 'NewsController::newsTypeMe/$1', ['filter' => 'Auth']);
+    // Cria a notícia
+    $routes->post('news/create', 'NewsController::create', ['filter' => 'Auth']);
+    // Altera a notícia
+    $routes->put('news/update/(:num)', 'NewsController::update/$1', ['filter' => 'Auth']);
+    // Deleta Tipo de notícia
+    $routes->delete('news/delete/(:num)', 'NewsController::delete/$1', ['filter' => 'Auth']);
 });
 
 
